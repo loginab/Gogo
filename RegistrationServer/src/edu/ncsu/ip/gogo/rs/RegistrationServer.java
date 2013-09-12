be0fb4973206ec20808a614e6e885ab7f5e46846
Abhay <loginabhay@gmail.com>
2013-09-12 19:22:25 -0400

    initial commit

---
+++ RegistrationServer/src/edu/ncsu/ip/gogo/rs/RegistrationServer.java
@@ -0,0 +1,13 @@
+package edu.ncsu.ip.gogo.rs;
+
+public class RegistrationServer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
