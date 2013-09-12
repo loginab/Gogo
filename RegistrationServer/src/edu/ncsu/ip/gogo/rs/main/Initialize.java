76613d7e0257bdcc1ab9f838703e9c022f9bc31a
Abhay <loginabhay@gmail.com>
2013-09-12 19:57:47 -0400

    server listening on a port

---
+++ RegistrationServer/src/edu/ncsu/ip/gogo/rs/main/Initialize.java
@@ -0,0 +1,18 @@
+package edu.ncsu.ip.gogo.rs.main;
+
+import edu.ncsu.ip.gogo.rs.server.RegistrationServer;
+
+public class Initialize {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		RegistrationServer rs = new RegistrationServer(65423);
+		rs.init();
+		
+
+	}
+
+}
---
+++ RegistrationServer/src/edu/ncsu/ip/gogo/rs/server/RegistrationServer.java
@@ -0,0 +1,39 @@
+package edu.ncsu.ip.gogo.rs.server;
+
+import java.net.Inet4Address;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.*;
+public class RegistrationServer {
+
+	private int port;
+	
+	public RegistrationServer(int prt) {
+		port = prt;
+		
+	}
+	
+	public void init()  {
+		
+		 ServerSocket serverSocket = null;
+	        try {
+	            serverSocket = new ServerSocket(port);
+	        } catch (IOException e) {
+	            System.err.println("Could not listen on port:"+ port);
+	            System.exit(1);
+	        }
+	 
+	        Socket clientSocket = null;
+	        try {
+	        	
+	        	System.out.println("The ip address is "+Inet4Address.getLocalHost().getHostAddress());
+	            clientSocket = serverSocket.accept();
+	        } catch (IOException e) {
+	            System.err.println("Accept failed.");
+	            System.exit(1);
+	        }
+		// TODO Auto-generated method stub
+
+	}
+
+}
